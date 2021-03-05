1990b814710937a0dd37b1f641e244228d5164ca
Luis Camacho <camacho.le@gmail>
2021-03-04 22:27:25 -0600

    [UNESCO] Lab 02 style was upgraded

--- js/conf/200_ACT/activity200.js
+++ js/conf/200_ACT/activity200.js
@@ -3,39 +3,39 @@ const activity2 = {
     {
       name: 'Gallina',
       title: 'La Gallina',
-      image: 'images/default.png',
+      image: 'images/Lab2/lab02-card-gallina.svg',
       code: 'gallina',
       cycle: [
         {
           step: 2,
           name: 'Crece y se desarrolla',
           code: 'ga-2',
-          image: 'images/default.png',
+          image: 'images/Lab2/lab02-icon-pollito-comiendo.svg',
         },
         {
           step: 4,
           name: 'Muere',
           code: 'ga-4 ',
-          image: 'images/default.png',
+          image: 'images/Lab2/lab02-icon-gallina-vieja.svg',
         },
         {
           step: 3,
           name: 'Se reproduce',
           code: 'ga-3',
-          image: 'images/default.png',
+          image: 'images/Lab2/lab02-icon-gallina-nido.svg',
         },
         {
           step: 1,
           name: 'Nace',
           code: 'ga-1',
-          image: 'images/default.png',
+          image: 'images/Lab2/lab02-icon-huevo-naciendo.svg',
         },
       ],
     },
     {
       name: 'Mariposa',
       title: 'La Mariposa',
-      image: 'images/default.png',
+      image: 'images/Lab2/lab02-card-mariposa.svg',
       code: 'mariposa',
       cycle: [
         {
@@ -69,6 +69,158 @@ const activity2 = {
           image: 'images/default.png',
         },
       ],
-    }
+    },
+    {
+      name: 'Mariposa',
+      title: 'La Mariposa',
+      image: 'images/Lab2/lab02-card-humanos.svg',
+      code: 'mariposa',
+      cycle: [
+        {
+          step: 1,
+          name: 'Nace',
+          code: 'ma-1',
+          image: 'images/default.png',
+        },
+        {
+          step: 2,
+          name: 'Crece y se desarrolla',
+          code: 'ma-2',
+          image: 'images/default.png',
+        },
+        {
+          step: 3,
+          name: '',
+          code: 'ma-3',
+          image: 'images/default.png',
+        },
+        {
+          step: 4,
+          name: 'Se reproduce',
+          code: 'ma-4 ',
+          image: 'images/default.png',
+        },
+        {
+          step: 5,
+          name: 'Muere',
+          code: 'ma-5 ',
+          image: 'images/default.png',
+        },
+      ],
+    },
+    {
+      name: 'Mariposa',
+      title: 'La Mariposa',
+      image: 'images/Lab2/lab02-card-arbol.svg',
+      code: 'mariposa',
+      cycle: [
+        {
+          step: 1,
+          name: 'Nace',
+          code: 'ma-1',
+          image: 'images/default.png',
+        },
+        {
+          step: 2,
+          name: 'Crece y se desarrolla',
+          code: 'ma-2',
+          image: 'images/default.png',
+        },
+        {
+          step: 3,
+          name: '',
+          code: 'ma-3',
+          image: 'images/default.png',
+        },
+        {
+          step: 4,
+          name: 'Se reproduce',
+          code: 'ma-4 ',
+          image: 'images/default.png',
+        },
+        {
+          step: 5,
+          name: 'Muere',
+          code: 'ma-5 ',
+          image: 'images/default.png',
+        },
+      ],
+    },
+    {
+      name: 'Mariposa',
+      title: 'La Mariposa',
+      image: 'images/Lab2/lab02-card-rana.svg',
+      code: 'mariposa',
+      cycle: [
+        {
+          step: 1,
+          name: 'Nace',
+          code: 'ma-1',
+          image: 'images/default.png',
+        },
+        {
+          step: 2,
+          name: 'Crece y se desarrolla',
+          code: 'ma-2',
+          image: 'images/default.png',
+        },
+        {
+          step: 3,
+          name: '',
+          code: 'ma-3',
+          image: 'images/default.png',
+        },
+        {
+          step: 4,
+          name: 'Se reproduce',
+          code: 'ma-4 ',
+          image: 'images/default.png',
+        },
+        {
+          step: 5,
+          name: 'Muere',
+          code: 'ma-5 ',
+          image: 'images/default.png',
+        },
+      ],
+    },
+    {
+      name: 'Mariposa',
+      title: 'La Mariposa',
+      image: 'images/Lab2/lab02-card-hongo.svg',
+      code: 'mariposa',
+      cycle: [
+        {
+          step: 1,
+          name: 'Nace',
+          code: 'ma-1',
+          image: 'images/default.png',
+        },
+        {
+          step: 2,
+          name: 'Crece y se desarrolla',
+          code: 'ma-2',
+          image: 'images/default.png',
+        },
+        {
+          step: 3,
+          name: '',
+          code: 'ma-3',
+          image: 'images/default.png',
+        },
+        {
+          step: 4,
+          name: 'Se reproduce',
+          code: 'ma-4 ',
+          image: 'images/default.png',
+        },
+        {
+          step: 5,
+          name: 'Muere',
+          code: 'ma-5 ',
+          image: 'images/default.png',
+        },
+      ],
+    },
   ],
 }
