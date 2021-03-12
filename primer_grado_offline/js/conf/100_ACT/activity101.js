6a74253c08dde4a6b92e78827118497eb8503246
Luis Camacho <camacho.le@gmail>
2021-03-11 22:31:16 -0600

    New folder structure

---
+++ primer_grado_offline/js/conf/100_ACT/activity101.js
@@ -0,0 +1,274 @@
+const activity101 = {
+  introSlides: [
+    // {
+    //  title: 'Los seres vivos y los elementos inertes',
+    //  image: "images/lab1/lab01-nina.svg",
+    //  audio: 'audios/correct-answer.mp3',
+    //  strings: [
+    //    '¡Hola!',
+    //    '¿Sabías que las plantas están vivas? Si, como vos y como yo.',
+    //    'Y que además existen los elementos inertes.',
+    //    '¿Querés que te explique?',
+    //   ],
+    // },
+    // {
+    //   title: 'Seleccioná un elemento inerte natural.',
+    //   image: "https://picsum.photos/800/600?grayscale",
+    //   audio: 'audios/correct-answer.mp3',
+    //   strings: [
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //   ],
+    // },
+    // {
+    //   title: 'Seleccioná un elemento inerte natural.',
+    //   image: "https://picsum.photos/800/600?grayscale",
+    //   audio: 'audios/correct-answer.mp3',
+    //   strings: [
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //   ],
+    // },
+    // {
+    //   title: 'Seleccioná un elemento inerte natural.',
+    //   image: "https://picsum.photos/800/600?grayscale",
+    //   audio: 'audios/correct-answer.mp3',
+    //   strings: [
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //     '¡Mmm..creo que estas un poco confundido! Las cercas son construidas por las personas, por lo que no son seres naturales.',
+    //   ],
+    // }
+  ],
+  slides: [
+    {
+      title: 'Seleccioná un elemento inerte natural.',
+      image: "images/lab1/lab01-landscape-bk-vacio.png",
+      audio: 'audios/01_element-inerte-natural.mp3',
+      options: [
+        {
+          name: '',
+          image: 'images/lab1/lab01-icon-cerca.svg',
+          isCorrect: false,
+          feedback: '¡Mmm... creo que estás un poco confundido! Las cercas de madera son construidas por las personas por lo que no son elementos naturales.',
+          audio: 'audios/01-feedback-1-cerca.mp3',
+        },
+        {
+          name: '',
+          image: 'images/lab1/lab01-icon-agua.svg',
+          isCorrect: true,
+          feedback: 'El agua es un elemento natural e inerte.',
+          audio: 'audios/01-feedback-1-Agua.mp3',
+        },
+        {
+          name: '',
+          image: 'images/lab1/lab01-icon-arbol.svg',
+          isCorrect: false,
+          feedback: 'Los árboles están vivos por lo que reaccionan, por ejemplo, sus raíces crecen hacia donde pueden encontrar agua.',
+          audio: 'audios/01-feedback-1-arbol.mp3',
+        },
+      ],
+    },
+    {
+      title: '¿Cuál de estos es un ser vivo?',
+      image: "images/lab1/lab01-landscape-conagua.png",
+      audio: 'audios/02_ser-vivo.mp3',
+      options: [
+        {
+          name: 'Agua',
+          image: 'images/lab1/lab01-icon-agua.svg',
+          isCorrect: false,
+          feedback: '¡Oh, oh! El agua es inerte, aunque indispensable para los seres vivos. ',
+          audio: 'audios/02-feedback-agua.mp3',
+        },
+        {
+          name: 'Delfín',
+          image: 'images/lab1/lab01-icon-delfin.svg',
+          isCorrect: true,
+          feedback: 'Sabías que los delfines respiran por los pulmones, no tienen branquias como otros seres que viven en el mar.',
+          audio: 'audios/02-feedback-delfin.mp3',
+        },
+        {
+          name: 'Suelo',
+          image: 'images/lab1/lab01-icon-tierra.svg',
+          isCorrect: false,
+          feedback: '¡No! El suelo es natural, tiene muchas condiciones que hacen posible la vida, pero en sí mismo no es un ser vivo.',
+          audio: 'audios/02-feedback-suelo.mp3',
+        },
+      ],
+    },
+    {
+      title: 'Ahora, seleccioná un ser que respira. ',
+      image: "images/lab1/lab01-landscape-delfin.png",
+      audio: 'audios/03-cual-respira.mp3',
+      options: [
+        {
+          name: 'Mono',
+          image: 'images/lab1/lab01-icon-mono.svg',
+          isCorrect: true,
+          feedback: '¡Muy bien! El mono es un ser vivo por lo que respira. Incluso, es pariente de los seres humanos.',
+          audio: 'audios/03-feedback-mono.mp3',
+        },
+        {
+          name: 'Piedra',
+          image: 'images/lab1/lab01-icon-piedra.svg',
+          isCorrect: false,
+          feedback: '¡Oh, oh! Si respirara estaría viva y las rocas son inertes.',
+          audio: 'audios/03-feedback-piedra.mp3',
+        },
+        {
+          name: 'Nube',
+          image: 'images/lab1/lab01-icon-nube.svg',
+          isCorrect: false,
+          feedback: 'Aunque las nubes anden por el aire, no lo respiran. Son objetos compuestos por gotas de agua. ',
+          audio: 'audios/03-feedback-nubes.mp3',
+        },
+      ],
+    },
+    {
+      title: 'Seleccioná un ser que crece.',
+      image: "images/lab1/lab01-landscape-mono.png",
+      audio: 'audios/04-ser-que-crece.mp3',
+      options: [
+        {
+          name: 'Sol',
+          image: 'images/lab1/lab01-icon-sol.svg',
+          isCorrect: false,
+          feedback: '¡Oh, oh! El Sol no es un ser vivo. Aunque sí ayuda al crecimiento de plantas y a la vida en la Tierra.',
+          audio: 'audios/04-feedback-sol.mp3',
+        },
+        {
+          name: 'Lápiz',
+          image: 'images/lab1/lab01-icon-lapiz.svg',
+          isCorrect: false,
+          feedback: 'Los lápices son seres inertes, o sea, no tienen vida.',
+          audio: 'audios/04-feedback-lapiz.mp3',
+        },
+        {
+          name: 'Árbol',
+          image: 'images/lab1/lab01-icon-arbol.svg',
+          isCorrect: true,
+          feedback: 'Los árboles respiran mediante sus hojas y producen su propio alimento al absorber nutrientes del suelo por sus raíces.',
+          audio: 'audios/04-feedback-arbol.mp3',
+        },
+      ],
+    },
+    {
+      title: 'Escogé un elemento inerte artificial.',
+      image: "images/lab1/lab01-landscape-arbol.png",
+      audio: 'audios/05-elemento-inerte-artificial.mp3',
+      options: [
+        {
+          name: 'Sol',
+          image: 'images/lab1/lab01-icon-sol.svg',
+          isCorrect: false,
+          feedback: '¡Casi, casi, pero no! El Sol sí es un ser inerte, sin embargo, es natural, o sea, no fue creado por el ser humano.',
+          audio: 'audios/05-feedback-sol.mp3',
+        },
+        {
+          name: 'Cerca',
+          image: 'images/lab1/lab01-icon-cerca.svg',
+          isCorrect: true,
+          feedback: '¡Muy bien! Las cercas de madera no tienen vida y son construidas por las personas.',
+          audio: 'audios/05-feedback-cerca.mp3',
+        },
+        {
+          name: 'Tortuga',
+          image: 'images/lab1/lab01-icon-tortuga.svg',
+          isCorrect: false,
+          feedback: 'Son lentas y a veces las podés confundir con una piedra, pero son seres vivos.',
+          audio: 'audios/05-feedback-tortuga.mp3',
+        },
+      ],
+    },
+    {
+      title: 'Seleccioná un ser vivo que ocupa aire para vivir, como vos y como yo, pero que aguanta la respiración bajo el agua',
+      image: "images/lab1/lab01-landscape-cerca.png",
+      audio: 'audios/06-ser-vivo-aire.mp3',
+      options: [
+        {
+          name: 'Tortuga marina',
+          image: 'images/lab1/lab01-icon-tortuga.svg',
+          isCorrect: true,
+          feedback: 'Sabías que las tortugas marinas cuando duermen pueden estar entre 4 y 7 horas bajo el agua.',
+          audio: 'audios/06-feedback-tortuga.mp3',
+        },
+        {
+          name: 'Pulpo',
+          image: 'images/lab1/lab01-icon-pulpo.svg',
+          isCorrect: false,
+          feedback: '¡No! Los pulpos son seres marinos que toman el oxígeno del agua. ',
+          audio: 'audios/06-feedback-pulpo.mp3',
+        },
+        {
+          name: 'Pez ',
+          image: 'images/lab1/lab01-icon-pez.svg',
+          isCorrect: false,
+          feedback: 'Los peces respiran bajo el agua, mediante unos órganos especiales llamados branquias con las cuales pueden filtrar el oxígeno del agua.',
+          audio: 'audios/06-feedback-pez.mp3',
+        },
+      ],
+    },
+    {
+      title: 'Volvamos nuestra mirada para arriba, ¿cuál es un elemento inerte natural que podemos ver en el cielo?',
+      image: "images/lab1/lab01-landscape-tortuga.png",
+      audio: 'audios/07-elemento-inerte-natural-cielo.mp3',
+      options: [
+        {
+          name: 'Avión',
+          image: 'images/lab1/lab01-icon-avion.svg',
+          isCorrect: false,
+          feedback: '¡Casi, casi, pero no! Acertaste una característica de dos. El avión es un elemento inerte artificial desarrollado por las personas.',
+          audio: 'audios/07-feedback-avion.mp3',
+        },
+        {
+          name: 'Lapa roja',
+          image: 'images/lab1/lab01-icon-laparoja.svg',
+          isCorrect: false,
+          feedback: 'Sí podemos ver lapas en el cielo mientras vuelan, pero son aves por lo tanto son seres vivos. ',
+          audio: 'audios/07-feedback-lapa-roja.mp3',
+        },
+        {
+          name: 'Sol ',
+          image: 'images/lab1/lab01-icon-sol.svg',
+          isCorrect: true,
+          feedback: 'Con su luz y calor ayuda al crecimiento de plantas y a la vida en la Tierra.',
+          audio: 'audios/07-feedback-sol.mp3',
+        },
+      ],
+    },
+    {
+      title: 'Por último, escogé un ser que se reproduce por medio de huevos. ',
+      image: "images/lab1/lab01-landscape-sol.png",
+      audio: 'audios/08-ser-vivo-huevos.mp3',
+      options: [
+        {
+          name: 'Lapa roja  ',
+          image: 'images/lab1/lab01-icon-laparoja.svg',
+          isCorrect: true,
+          feedback: '¡Excelente respuesta! Todos los pájaros son ovíparos,  es decir, nacen de huevos.',
+          audio: 'audios/08-feedback-lapa-roja.mp3',
+        },
+        {
+          name: 'Delfín',
+          image: 'images/lab1/lab01-icon-delfin.svg',
+          isCorrect: false,
+          feedback: 'Los delfines son mamíferos por lo que nacen directamente de sus madres.',
+          audio: 'audios/08-feedback-delfines.mp3',
+        },
+        {
+          name: 'Piedra ',
+          image: 'images/lab1/lab01-icon-piedra.svg',
+          isCorrect: false,
+          feedback: '¡Oh, oh! Para reproducirse debería tener vida y las rocas son inertes.',
+          audio: 'audios/08-feedback-piedra.mp3',
+        },
+      ],
+    },
+
+
+
+  ],
+}
---
+++ tercer_grado_offline/js/conf/300_ACT/activity300.js
@@ -0,0 +1,52 @@
+const activity3 = {
+  organs: [
+    {
+      name: 'Boca',
+      code: 'boca',
+      description: 'Es el lugar de entrada de los alimentos al organismo. En ella se encuentran los dientes que con ayuda de las glándulas salivales y la lengua  trituran los alimentos.',
+      selected: false
+    },
+    {
+      name: 'Faringe',
+      code: 'faringe',
+      description: 'Canal que permite la comunicación entre la boca y el esófago. También forma parte del sistema respiratorio.',
+      selected: false
+    },
+    {
+      name: 'Esófago',
+      code: 'esofago',
+      description: 'Canal que permite la comunicación entre la boca y el esófago. También forma parte del sistema respiratorio.',
+      selected: false
+    },
+    {
+      name: 'Estómago',
+      code: 'estomago',
+      description: 'Es una bolsa musculosa en forma de J. En él se secreta el jugo gástrico.',
+      selected: false
+    },
+    {
+      name: 'Hígado',
+      code: 'higado',
+      description: 'Es un órgano que segrega bilis, una sustancia que ayuda a digerir las grasas.',
+      selected: false
+    },
+    {
+      name: 'Páncreas',
+      code: 'pancreas',
+      description: 'Es un órgano que segrega jugo pancreático, el cual ayuda en la digestión de azúcares, grasas y proteínas.',
+      selected: false
+    },
+    {
+      name: 'Intestino delgado',
+      code: 'int-delgado',
+      description: 'Es donde se produce la mayor parte de la digestión, ya que junto con la bilis y el jugo pancreático, se encarga de absorber los nutrientes de los alimentos.',
+      selected: false
+    },
+    {
+      name: 'Intestino grueso',
+      code: 'int-grueso',
+      description: 'Es un tubo grueso y corto que rodea al intestino delgado. Se encarga de expulsar los residuos al exterior del cuerpo.',
+      selected: false
+    },
+  ],
+}
